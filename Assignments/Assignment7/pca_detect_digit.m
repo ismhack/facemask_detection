89f559c6829f083f2cf0a19b02798e3140b89a12
Ismael <ismhack@msn.com>
2021-03-13 11:56:41 -0600

    pca detect digit working 100%

--- Assignments/Assignment7/pca_detect_digit.m
+++ Assignments/Assignment7/pca_detect_digit.m
@@ -6,19 +6,19 @@ window = zeros(trows, tcols);
 result = realmax;
 row = 0;
 col =0;
-disp(rows);
 for i = 1:rows
     for j = 1:cols
         xrow = i + trows -1;
         ycol = j + tcols -1;
         if xrow <= rows && ycol <= cols
             window = image(i: xrow, j: ycol);
+            %figure(1); imshow(reshape(window, [trows, tcols]), []);
             temp = pca_score(window, mean_digit, eigenvectors, N);
-            fprintf('Checking window row:%d-%d  col:%d-%d,  result:%f.2 \n',i, xrow,j, ycol, temp);
+            %fprintf('Checking window row:%d-%d  col:%d-%d,  result:%f.2 \n',i, xrow,j, ycol, temp);
             if(temp < result)
                result = temp;
-               col = ceil((i + xrow)/2);
-               row = ceil((j + ycol)/2);
+               row = ceil((i + xrow)/2);
+               col = ceil((j + ycol)/2);
             end    
         end
     end
--- Assignments/Assignment7/task1.m
+++ Assignments/Assignment7/task1.m
@@ -11,17 +11,17 @@ total3_count = size(find(mnist_labels ==3),1);
 digits2 = zeros(rows*cols, total2_count);
 digits3 = zeros(rows*cols, total3_count);
 % set mean of all faces to 0, and std to 1.
-count=1;
-for index = find(mnist_labels==2)
+i=1;
+for index = find(mnist_labels==2)'
         digit = mnist_digits(:,:, index);
         digit = digit(:);
         digit = (digit - mean(digit)) / std(digit);
-        digits2(:, count) = digit;
-        count= count+1;
+        digits2(:, i) = digit;
+        i= i+1;
 end
 
 count=1;
-for index = find(mnist_labels==3)
+for index = find(mnist_labels==3)'
         digit = mnist_digits(:,:, index);
         digit = digit(:);
         digit = (digit - mean(digit)) / std(digit);
@@ -31,7 +31,7 @@ end
 
 
 
-[averag2, eigenvectors2, eigenvalues2] = compute_pca(digits2); 
+[average2, eigenvectors2, eigenvalues2] = compute_pca(digits2); 
 figure(1); imshow(reshape(eigenvectors2(:, 1),rows, cols), []);
 figure(2); imshow(reshape(eigenvectors2(:, 2),rows, cols), []);
 figure(3); imshow(reshape(eigenvectors2(:, 3),rows, cols), []);
@@ -48,36 +48,26 @@ figure(9); imshow(reshape(eigenvectors3(:, 4),rows, cols), []);
 figure(10); imshow(reshape(eigenvectors3(:, 5),rows, cols), []);
 
 
-%% TEST detect digit
-image = read_gray('new_three20.bmp');
-[row, col, result] = pca_detect_digit(image, reshape(average3, [rows cols]), eigenvectors3, 10);
+%% TEST 3 detect digit
+for i = 1:20
+    image = read_gray(['new_three' num2str(i,'%d') '.bmp']);
+    %image = read_gray('new_three1.bmp');
+    [row, col, result] = pca_detect_digit(image, reshape(average3, [rows cols]), eigenvectors3, 10);
 
-figure(3); imshow(draw_rectangle2(image, row, col, rows, cols), []);
-
-%% TEST recognize digit all test data
-correct =0;
-wrong=0;
-for i = 1:40
-    test = read_gray(['test' num2str(i,'%d') '.bmp']);
-    digit = recognize_digit(test, average2, average3);
-    if( i < 20)
-        if( digit == 2)
-            correct = correct +1;
-        else
-            wrong = wrong + 1;
-            fprintf('failed test%d.bmp \n',i);
-        end
-    else
-         if( digit == 3)
-            correct = correct +1;
-        else
-            wrong = wrong + 1;
-            fprintf('failed test%d.bmp \n',i);
-         end
-    end
+    figure(i); imshow(draw_rectangle2(image, row, col, rows, cols), []);
 
 end
 
-fprintf('recognize_digit: Correct %d - Failed: %d - Accuracy: %.2f ', correct,wrong, (correct/40)* 100);
+%% TEST 2 detect digit 
+for i = 1:20
+    image = read_gray(['new_two' num2str(i,'%d') '.bmp']);
+    %image = read_gray('new_three1.bmp');
+    [row, col, result] = pca_detect_digit(image, reshape(average2, [rows cols]), eigenvectors2, 10);
+
+    figure(i); imshow(draw_rectangle2(image, row, col, rows, cols), []);
+
+end
+
+%fprintf('recognize_digit: Correct %d - Failed: %d - Accuracy: %.2f ', correct,wrong, (correct/40)* 100);
 %%
 
