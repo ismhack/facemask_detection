58abe9b8fd3013f3ffa21f85da997d686204a02d
Ismael <ismhack@msn.com>
2021-04-04 22:13:49 -0500

    assignment 9 complete, pending readme

--- Assignments/Assignment9/cnn_detector.m
+++ Assignments/Assignment9/cnn_detector.m
@@ -1,26 +1,34 @@
-function results = cnn_detector(image, model, face_size, number_result)
+function scores = cnn_detector(image, model, face_size)
 
 [rows, cols] = size(image);
-[trows, tcols] = face_size;
+trows = face_size(1);
+tcols = face_size(2);
 window = zeros(trows, tcols);
-scores = zeros(number_results) + realmax;
+scores = zeros(rows, cols);
 row = 0;
 col =0;
 
-for i = 1:rows
-    for j = 1:cols
+for i = 1:2:rows
+    for j = 1:2:cols
         xrow = i + trows -1;
         ycol = j + tcols -1;
         if xrow <= rows && ycol <= cols
             window = image(i: xrow, j: ycol);
             %figure(1); imshow(reshape(window, [trows, tcols]), []);
-            temp = net.predict(window);
-            %fprintf('Checking window row:%d-%d  col:%d-%d,  result:%f.2 \n',i, xrow,j, ycol, temp);
-            if(temp < score)
-               score = temp;
-               row = floor((i + xrow)/2);
-               col = floor((j + ycol)/2);
-            end    
+            score = model.predict(window);
+            %fprintf('Checking window row:%d-%d  col:%d-%d,  result:%f.2 \n',i, xrow,j, ycol, score(2));
+           row = floor((i + xrow)/2);
+           col = floor((j + ycol)/2);
+           
+           scores(row, col) = score(2);
+           %scores(i * j, 2) = row;
+           %scores(i * j, 3) = col;
+  
         end
     end
 end
+
+%figure(1); imshow(image, []);
+
+
+
