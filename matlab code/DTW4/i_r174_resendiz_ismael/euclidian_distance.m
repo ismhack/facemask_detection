65dd81135d9d41464f4bc752157bc4cf9b9cf50b
Ismael <ismhack@msn.com>
2022-04-16 13:23:01 -0500

    removing matlab examples

--- matlab code/DTW4/i_r174_resendiz_ismael/euclidian_distance.m
+++
@@ -1,5 +0,0 @@
-function result = euclidian_distance(x, y)
-
-result = sqrt((x(1)-y(1))^2+(x(2)-y(2))^2);
-%result = abs(x(1) - x(2)) + abs(y(1) - y(2));
-
--- matlab code/Digit recognition/i_r174_resendiz_ismael/detect_digit.m
+++
@@ -1,8 +0,0 @@
-function [row, col] =  detect_digit(image, template)
-[h, w] = size(template);
-result = normalized_correlation(image, template);
-
-maxVal = max(result(:));
-[row, col] = find(result == maxVal);
-
-figure(1); imshow(draw_rectangle2(image, row, col, h, w), []);
